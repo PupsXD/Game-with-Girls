2156c2e65bdd3b3b6e14264a0617deb4dbce3bc0
Petr Sokolov <74403997+PupsXD@users.noreply.github.com>
2024-05-08 13:22:39 +0300

    Safe commit adding adressables

---
+++ ProjectForIdor/Assets/Scripts/Adressables/AdressableInitialization.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class AdressableInitialization : MonoBehaviour
+{
+    [SerializeField] private AssetReference girlSO;
+
+    [SerializeField] private AssetReference girlPage;
+
+    [SerializeField] private AssetReference girlPageCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
