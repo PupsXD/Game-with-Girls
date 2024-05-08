7cd27baefd3eac42f38a1a4f4e94c69da61a0c9d
Petr Sokolov <74403997+PupsXD@users.noreply.github.com>
2024-05-08 12:21:50 +0300

    Girl page done

---
+++ ProjectForIdor/Assets/Scripts/MVP_Girl_Page/GirlPageModel.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using ScriptableObjects.Girls;
+using UnityEngine;
+
+public class GirlPageModel : MonoBehaviour
+{
+    private GirlSO girlSO;
+
+    public void SetGirlSO(GirlSO girlSO)
+    {
+        this.girlSO = girlSO;
+    }
+    
+    public GirlSO GetGirlSO()
+    {
+        return girlSO;
+    }
+      
+   
+}
